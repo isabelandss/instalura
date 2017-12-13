8b17819e50741304c06ad719ffc293fb07096b16
Isabela Neri dos Santos <isabela.santos@fcamara.com.br>
2017-12-13 12:26:06 -0200

    Desenvolvendo profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -9,36 +9,63 @@ import {
     FlatList,
     Button,
     TouchableOpacity } from 'react-native'
+    
+import { MaterialIcons, Ionicons, Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
 
 export default class Profile extends React.Component { 
     render() {
         return(
             <View>
-                <View style={ styles.wrapper } >
-                    <Image style={ styles.photoProfile } source={ require("../../resources/img/profile.jpg") }/>
-                    <View style={ styles.column }>
-                        <View style={ styles.row }>
-                            <Text>90</Text>
-                            <Text>589</Text>
-                            <Text>892</Text>
+                <View style={{flexDirection: "row"}}>
+                    <View style={{ flexDirection: "column", marginRight: 0, marginTop: 18}}>
+                        <Image style={ styles.photoProfile } source={ require("../../resources/img/profile.jpg") }/>
+                    </View>
+                    <View style={{ flexDirection: "column", marginTop: 55, marginLeft: 0}}>    
+                        <View style={{ flexDirection: "row", justifyContent: "space-around"}}>
+                            <View style={{ flexDirection: "column", marginRight: 8, marginLeft: 10}}>
+                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>90</Text>
+                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>publicações</Text>
+                            </View>
+                            <View style={{ flexDirection: "column", marginLeft: 4, marginRight: 8}}>
+                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>538</Text>
+                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>seguidores</Text>
+                            </View>
+                            <View style={{ flexDirection: "column", marginLeft: 4, marginRight: 4}}>
+                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>364</Text>
+                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>seguindo</Text>
+                            </View>
                         </View>
-                        <View style={ styles.row }>
-                            <Text>publicações</Text>
-                            <Text>seguidores</Text>
-                            <Text>seguindo</Text>
-                        </View>  
-                        <TouchableOpacity
-                            style={styles.button}
-                            onPress={() => {}}>
-                            <Text>Editar seu perfil</Text>
-                        </TouchableOpacity>  
+                        <View style={{ flexDirection: "row", justifyContent: "space-around"}}>
+                            <TouchableOpacity
+                                style={{backgroundColor: "white", marginTop: 10, borderColor: "#ccc", borderRadius: 4, borderWidth: 1}}
+                                onPress={() => {}}>
+                                <Text style={{ fontWeight: "bold", paddingTop: 3, paddingBottom: 3, paddingLeft: 60, paddingRight: 60}}>Editar seu perfil</Text>
+                            </TouchableOpacity>  
+                        </View>
                     </View>
                 </View>
-                <View style={ styles.info }>
-                    <Text>Isabela Neri</Text>
-                    <Text>Brazil</Text>
-                    <Text>21 years old</Text>
-                    <Text>https://www.facebook.com/IsabelaNerii</Text>
+                <View style={{flexDirection: "row"}}>
+                    <View style={ styles.info }>
+                        <Text style={{fontWeight: 'bold'}}>Isabela Neri</Text>
+                        <Text>Brazil</Text>
+                        <Text>21 years old</Text>
+                        <Text>www.facebook.com/IsabelaNeri</Text>
+                    </View>
+                </View>
+
+                <View style={{flexDirection: "row", borderWidth: 0.5, borderColor: "#D3D3D3", height: 55, justifyContent: "space-around"}}>
+                    <View style={{ flexDirection: "column"}}>
+                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+                    </View>
+                    <View style={{ flexDirection: "column"}}>
+                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+                    </View>
+                    <View style={{ flexDirection: "column"}}>
+                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+                    </View>
+                    <View style={{ flexDirection: "column"}}>
+                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+                    </View>
                 </View>
             </View>
         )
@@ -47,10 +74,12 @@ export default class Profile extends React.Component {
 
 var styles = StyleSheet.create({
     photoProfile: {
-        width: 90,
-        height: 90,
-        borderRadius: 45,
+        width: 80,
+        height: 80,
+        borderRadius: 40,
         margin: 20,
+        marginRight: 10,
+        marginLeft: 15,
         marginTop: 40
     },
     wrapper: {
@@ -58,6 +87,10 @@ var styles = StyleSheet.create({
     },
     row: {
         flexDirection: "row"
+    },
+    rowSpace: {
+        flexDirection: "row",
+        justifyContent: "center",
     },
     column: {
         flexDirection: "column",
