ab04c79cc84aed0361d3ba1b5e2cb540d99ae646
Isabela Neri dos Santos <isabela.santos@fcamara.com.br>
2017-12-14 10:31:38 -0200

    Adicionando fotos no profile e refatoração de código

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -8,71 +8,176 @@ import {
     ScrollView, 
     FlatList,
     Button,
-    TouchableOpacity } from 'react-native'
+    TouchableOpacity,
+    ActionSheetIOS,
+    CameraRoll 
+} from 'react-native'
     
-import { MaterialIcons, Ionicons, Entypo, MaterialCommunityIcons } from '@expo/vector-icons'
+import { 
+    MaterialIcons, 
+    Ionicons, 
+    Entypo, 
+    MaterialCommunityIcons 
+} from '@expo/vector-icons'
+
 
 export default class Profile extends React.Component { 
     render() {
         return(
-            <View>
-                <View style={{flexDirection: "row"}}>
-                    <View style={{ flexDirection: "column", marginRight: 0, marginTop: 18}}>
-                        <Image style={ styles.photoProfile } source={ require("../../resources/img/profile.jpg") }/>
-                    </View>
-                    <View style={{ flexDirection: "column", marginTop: 55, marginLeft: 0}}>    
-                        <View style={{ flexDirection: "row", justifyContent: "space-around"}}>
-                            <View style={{ flexDirection: "column", marginRight: 8, marginLeft: 10}}>
-                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>90</Text>
-                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>publicações</Text>
-                            </View>
-                            <View style={{ flexDirection: "column", marginLeft: 4, marginRight: 8}}>
-                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>538</Text>
-                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>seguidores</Text>
-                            </View>
-                            <View style={{ flexDirection: "column", marginLeft: 4, marginRight: 4}}>
-                                <Text style={{fontWeight: 'bold', textAlign: "center", fontSize: 20}}>364</Text>
-                                <Text style={{marginLeft: 0, textAlign: "center", color: "#A9A9A9"}}>seguindo</Text>
-                            </View>
-                        </View>
-                        <View style={{ flexDirection: "row", justifyContent: "space-around"}}>
-                            <TouchableOpacity
-                                style={{backgroundColor: "white", marginTop: 10, borderColor: "#ccc", borderRadius: 4, borderWidth: 1}}
-                                onPress={() => {}}>
-                                <Text style={{ fontWeight: "bold", paddingTop: 3, paddingBottom: 3, paddingLeft: 60, paddingRight: 60}}>Editar seu perfil</Text>
-                            </TouchableOpacity>  
-                        </View>
-                    </View>
-                </View>
-                <View style={{flexDirection: "row"}}>
-                    <View style={ styles.info }>
-                        <Text style={{fontWeight: 'bold'}}>Isabela Neri</Text>
-                        <Text>Brazil</Text>
-                        <Text>21 years old</Text>
-                        <Text>www.facebook.com/IsabelaNeri</Text>
-                    </View>
-                </View>
+            <ScrollView>
+                    <View style={ styles._row }>
 
-                <View style={{flexDirection: "row", borderWidth: 0.5, borderColor: "#D3D3D3", height: 55, justifyContent: "space-around"}}>
-                    <View style={{ flexDirection: "column"}}>
-                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+                        <View style={ styles.divPhoto }>
+                            <Image style={ styles.photoProfile } source={ require("../../resources/img/profile.jpg") }/>
+                        </View>
+
+                        <View style={ styles.divInfo }>    
+                            <View style={ styles.row }>
+                                <View style={ styles.publicacao }>
+                                    <Text style={ styles.number }>90</Text>
+                                    <Text style={ styles.info }>publicações</Text>
+                                </View>
+                                <View style={ styles.seguidor }>
+                                    <Text style={ styles.number }>538</Text>
+                                    <Text style={ styles.info }>seguidores</Text>
+                                </View>
+                                <View style={ styles.seguindo }>
+                                    <Text style={ styles.number }>364</Text>
+                                    <Text style={ styles.info }>seguindo</Text>
+                                </View>
+                            </View>
+                            <View style={ styles.row }>
+                                <TouchableOpacity
+                                    style={ styles.editProfile }
+                                    onPress={() => {}}>
+                                    <Text style={ styles.textEditProfile }>Editar seu perfil</Text>
+                                </TouchableOpacity>  
+                            </View>
+                        </View>
+
                     </View>
-                    <View style={{ flexDirection: "column"}}>
-                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+
+                    <View style={ styles._row }>
+                        <View style={ styles.description }>
+                            <Text style={ styles.bold }>Isabela Neri</Text>
+                            <Text>Brazil</Text>
+                            <Text>21 years old</Text>
+                            <Text>www.facebook.com/IsabelaNeri</Text>
+                        </View>
                     </View>
-                    <View style={{ flexDirection: "column"}}>
-                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+
+                    <View style={ styles.tabs }>
+                        <View style={ styles._column }>
+                            <Ionicons name='ios-list' size={45} style={ styles.icons } />
+                        </View>
+                        <View style={ styles._column }>
+                            <Ionicons name='ios-list' size={45} style={ styles.icons } />
+                        </View>
+                        <View style={ styles._column }>
+                            <Ionicons name='ios-list' size={45} style={ styles.icons } />
+                        </View>
+                        <View style={ styles._column }>
+                            <Ionicons name='ios-list' size={45} style={ styles.icons } />
+                        </View>
                     </View>
-                    <View style={{ flexDirection: "column"}}>
-                        <Ionicons name='ios-list' size={45} style={{ marginTop: 5 }} />
+
+                    <View style={ styles._row }>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto1.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto2.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto3.jpg") }/>
                     </View>
-                </View>
-            </View>
+                    <View style={ styles._row }>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto4.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto5.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto6.jpg") }/>
+                    </View>
+                    <View style={ styles._row }>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto7.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto8.jpg") }/>
+                        <Image style={ styles.photo } source={ require("../../resources/img/foto9.jpg") }/>
+                    </View>
+            </ScrollView>
         )
     }
 }
 
 var styles = StyleSheet.create({
+    _row: {
+        flexDirection: "row",
+    },
+    _column: { 
+        flexDirection: "column"
+    },
+    row: { 
+        flexDirection: "row", 
+        justifyContent: "space-around" 
+    },
+    number: {
+        fontWeight: 'bold', 
+        textAlign: "center", 
+        fontSize: 20
+    },
+    info:{
+        marginLeft: 0, 
+        textAlign: "center", 
+        color: "#A9A9A9"
+    },
+    description: {
+        margin: 20,
+        marginTop: 5
+    },
+    divPhoto: { 
+        flexDirection: "column", 
+        marginRight: 0, 
+        marginTop: 18
+    },
+    divInfo: { 
+        flexDirection: "column", 
+        marginTop: 55, 
+        marginLeft: 0
+    },
+    tabs: {
+        flexDirection: "row", 
+        borderWidth: 0.5, 
+        borderColor: "#D3D3D3", 
+        height: 55, 
+        justifyContent: "space-around"
+    },
+    icons: {
+        marginTop: 5
+    },
+    publicacao: { 
+        flexDirection: "column", 
+        marginRight: 8, 
+        marginLeft: 10
+    },
+    seguidor: { 
+        flexDirection: "column", 
+        marginLeft: 4, 
+        marginRight: 8
+    },
+    seguindo: { 
+        flexDirection: "column", 
+        marginLeft: 4, 
+        marginRight: 4
+    },
+    editProfile: {
+        backgroundColor: "white", 
+        marginTop: 10, 
+        borderColor: "#ccc", 
+        borderRadius: 4, 
+        borderWidth: 1
+    },
+    textEditProfile: { 
+        fontWeight: "bold", 
+        paddingTop: 3, 
+        paddingBottom: 3, 
+        paddingLeft: 60, 
+        paddingRight: 60
+    },
+    bold: {
+        fontWeight: 'bold'
+    },
     photoProfile: {
         width: 80,
         height: 80,
@@ -82,25 +187,8 @@ var styles = StyleSheet.create({
         marginLeft: 15,
         marginTop: 40
     },
-    wrapper: {
-        flexDirection: "row"
+    photo: {
+        width: 120,
+        height: 120,
     },
-    row: {
-        flexDirection: "row"
-    },
-    rowSpace: {
-        flexDirection: "row",
-        justifyContent: "center",
-    },
-    column: {
-        flexDirection: "column",
-        marginTop: 55
-    },
-    button: {
-        backgroundColor: "white"
-    },
-    info: {
-        margin: 20,
-        marginTop: 5
-    }
 })
